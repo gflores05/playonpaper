1c477c1168298e0869f33d3a732049683db14a48
gflores05 <gabriel.flores0503@gmail.com>
2021-10-08 11:59:38 -0600

    feat(socket): create client and connect to socket

---
+++ client/src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { io } from 'socket.io-client';
+
+import logo from './logo.svg';
+import './App.css';
+
+const socket = io(process.env.REACT_APP_SOCKET_SERVER!);
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
